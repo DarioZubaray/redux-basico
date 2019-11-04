09466293351efe3324624141b1fb4a9c27a6f4d7
dario.zubaray <dzubaray@imagosur.com>
2019-11-04 08:57:47 -0300

    Acciones y reducer basico

---
+++ README.md
@@ -0,0 +1,19 @@
+# Redux basico
+
+- Documentación
+
+(Link inglés)[https://redux.js.org/]
+
+- Ejecución:
+```
+npm run start
+ó
+.\node_modules\.bin\ts-node app.ts
+```
+
+- Dependencias de desarrollo o globales
+
+```
+"ts-node": "^8.4.1"
+"typescript": "^3.6.4"
+```
--- app.ts
+++ app.ts
@@ -1,1 +1,22 @@
-console.log('hola mundo');
+// Acciones
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+function reducer(state = 10, action: Action) {
+
+    if (action.type === 'INCREMENTAR') {
+        return state += 1;
+    }
+
+    return state;
+}
+
+
+// Usar el reducer
+const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+};
+
+console.log( reducer(10, incrementadorAction));
